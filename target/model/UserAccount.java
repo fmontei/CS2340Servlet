e87a174aa9ed575273eb9a98010d87ca58b4cb9d
fmontei <fmonteiro6@gatech.edu>
2014-06-05 19:06:52 -0400

    Added ant deploy.xml file/css/jsp files.

---
+++ target/model/UserAccount.java
@@ -0,0 +1,27 @@
+package model;
+
+public class UserAccount {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    public UserAccount(String first, String last, String username, String password) {
+        this.firstName = first;
+        this.lastName = last;
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean allFieldsHaveValue() {
+        return hasValue(firstName) && hasValue(lastName) && hasValue(username) && hasValue(password);
+    }
+
+    private boolean hasValue(String field) {
+        return field != null && !field.isEmpty();
+    }
+
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getName() { return firstName + " " + lastName; }
+}
