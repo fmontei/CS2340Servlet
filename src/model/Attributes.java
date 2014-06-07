2a7f03719cc165c097107d81f59bcf68c2e5a608
fmontei <fmontei@g.clemson.edu>
2014-06-06 22:12:34 -0400

    Nothing.

--- src/model/Attributes.java
+++ src/model/Attributes.java
@@ -8,8 +8,8 @@ public class Attributes {
     private static Map<String, String> map = new HashMap<String, String>();
 
     public static String getAttribute(String attribute) {
-        String value = map.get(attribute);
-        return value;
+        String found = map.get(attribute);
+        return found;
     }
 
     public static void storeAttribute(String attribute, String value) {
